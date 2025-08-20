3772e8ffd11180c5520243cb1a4c9855ea548083
Deki Peltshog <deki@steeleconsult.com>
2025-08-20 08:50:53 -0600

    moved AspStuff tp IdentityDbContext

--- Authentication/Models/IdentityDbContext.cs
+++ Authentication/Models/IdentityDbContext.cs
@@ -18,6 +18,15 @@ public class IdentityDbContext : IdentityDbContext<ApplicationUser, Role, string
 
     public virtual DbSet<AspNetUserExtension> AspNetUserExtensions { get; set; }
 
+    //public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
+    //public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
+
+    //public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+
+    // virtual DbSet<AspNetUserExtension> AspNetUserExtensions { get; set; }
+
+    //public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
 
     public IdentityDbContext(
         DbContextOptions<IdentityDbContext> options,
@@ -46,7 +55,7 @@ public class IdentityDbContext : IdentityDbContext<ApplicationUser, Role, string
             entity.Property(e => e.CompanyName).HasMaxLength(200);
             entity.Property(e => e.JobTitle).HasMaxLength(100);
         });
-        
+
         builder.Entity<AspNetUserExtension>(entity =>
         {
             entity.ToTable(tb => tb.HasTrigger("AspNetUserExtensions_ClearExpired"));
@@ -59,5 +68,92 @@ public class IdentityDbContext : IdentityDbContext<ApplicationUser, Role, string
             //     .OnDelete(DeleteBehavior.ClientSetNull)
             //     .HasConstraintName("FK_AspNetUserExtensions_AspNetUsers");
         });
+        
+        /*
+        modelBuilder.Entity<AspNetRole>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetRoles");
+
+            entity.HasIndex(e => e.Name, "RoleNameIndex").IsUnique();
+
+            entity.Property(e => e.Id).HasMaxLength(128);
+            entity.Property(e => e.Name).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<AspNetUser>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetUsers");
+
+            entity.ToTable(tb => tb.HasTrigger("AspNetUsers_FlagResetPassword_I"));
+
+            entity.HasIndex(e => e.UserName, "UserNameIndex").IsUnique();
+
+            entity.Property(e => e.Id).HasMaxLength(128);
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.ResetPasswordOnLoginTf).HasColumnName("ResetPasswordOnLoginTF");
+            entity.Property(e => e.UserName).HasMaxLength(256);
+
+            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
+                .UsingEntity<Dictionary<string, object>>(
+                    "AspNetUserRole",
+                    r => r.HasOne<AspNetRole>().WithMany()
+                        .HasForeignKey("RoleId")
+                        .HasConstraintName("FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId"),
+                    l => l.HasOne<AspNetUser>().WithMany()
+                        .HasForeignKey("UserId")
+                        .HasConstraintName("FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId"),
+                    j =>
+                    {
+                        j.HasKey("UserId", "RoleId").HasName("PK_dbo.AspNetUserRoles");
+                        j.ToTable("AspNetUserRoles");
+                        j.HasIndex(new[] { "RoleId" }, "IX_RoleId");
+                        j.HasIndex(new[] { "UserId" }, "IX_UserId");
+                        j.IndexerProperty<string>("UserId").HasMaxLength(128);
+                        j.IndexerProperty<string>("RoleId").HasMaxLength(128);
+                    });
+        });
+
+        modelBuilder.Entity<AspNetUserClaim>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetUserClaims");
+
+            entity.HasIndex(e => e.UserId, "IX_UserId");
+
+            entity.Property(e => e.UserId).HasMaxLength(128);
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId");
+        });
+
+        modelBuilder.Entity<AspNetUserLogin>(entity =>
+        {
+            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey, e.UserId }).HasName("PK_dbo.AspNetUserLogins");
+
+            entity.HasIndex(e => e.UserId, "IX_UserId");
+
+            entity.Property(e => e.LoginProvider).HasMaxLength(128);
+            entity.Property(e => e.ProviderKey).HasMaxLength(128);
+            entity.Property(e => e.UserId).HasMaxLength(128);
+
+            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId");
+        });
+
+        modelBuilder.Entity<AspNetUserExtension>(entity =>
+        {
+            entity.ToTable(tb => tb.HasTrigger("AspNetUserExtensions_ClearExpired"));
+
+            entity.Property(e => e.Id).HasMaxLength(128);
+            entity.Property(e => e.PasswordResetTokenExpiration).HasColumnType("datetime");
+
+            entity.HasOne(d => d.IdNavigation).WithOne(p => p.AspNetUserExtension)
+                .HasForeignKey<AspNetUserExtension>(d => d.Id)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_AspNetUserExtensions_AspNetUsers");
+        });
+        */
+
     }
 }
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -22,16 +22,6 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<AdminUser> AdminUsers { get; set; }
 
-    //public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
-
-    //public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
-
-    //public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
-
-    public virtual DbSet<AspNetUserExtension> AspNetUserExtensions { get; set; }
-
-    //public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
-
     public virtual DbSet<Document> Documents { get; set; }
 
     public virtual DbSet<DocumentsVw> DocumentsVws { get; set; }
@@ -119,91 +109,6 @@ public partial class AppDbContext : DbContext
                     .HasConstraintName("FK_AdminUsers_AspNetUsers");
         });
 
-/*
-        modelBuilder.Entity<AspNetRole>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetRoles");
-
-            entity.HasIndex(e => e.Name, "RoleNameIndex").IsUnique();
-
-            entity.Property(e => e.Id).HasMaxLength(128);
-            entity.Property(e => e.Name).HasMaxLength(256);
-        });
-
-        modelBuilder.Entity<AspNetUser>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetUsers");
-
-            entity.ToTable(tb => tb.HasTrigger("AspNetUsers_FlagResetPassword_I"));
-
-            entity.HasIndex(e => e.UserName, "UserNameIndex").IsUnique();
-
-            entity.Property(e => e.Id).HasMaxLength(128);
-            entity.Property(e => e.Email).HasMaxLength(256);
-            entity.Property(e => e.ResetPasswordOnLoginTf).HasColumnName("ResetPasswordOnLoginTF");
-            entity.Property(e => e.UserName).HasMaxLength(256);
-
-            entity.HasMany(d => d.Roles).WithMany(p => p.Users)
-                .UsingEntity<Dictionary<string, object>>(
-                    "AspNetUserRole",
-                    r => r.HasOne<AspNetRole>().WithMany()
-                        .HasForeignKey("RoleId")
-                        .HasConstraintName("FK_dbo.AspNetUserRoles_dbo.AspNetRoles_RoleId"),
-                    l => l.HasOne<AspNetUser>().WithMany()
-                        .HasForeignKey("UserId")
-                        .HasConstraintName("FK_dbo.AspNetUserRoles_dbo.AspNetUsers_UserId"),
-                    j =>
-                    {
-                        j.HasKey("UserId", "RoleId").HasName("PK_dbo.AspNetUserRoles");
-                        j.ToTable("AspNetUserRoles");
-                        j.HasIndex(new[] { "RoleId" }, "IX_RoleId");
-                        j.HasIndex(new[] { "UserId" }, "IX_UserId");
-                        j.IndexerProperty<string>("UserId").HasMaxLength(128);
-                        j.IndexerProperty<string>("RoleId").HasMaxLength(128);
-                    });
-        });
-
-        modelBuilder.Entity<AspNetUserClaim>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PK_dbo.AspNetUserClaims");
-
-            entity.HasIndex(e => e.UserId, "IX_UserId");
-
-            entity.Property(e => e.UserId).HasMaxLength(128);
-
-            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserClaims)
-                .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK_dbo.AspNetUserClaims_dbo.AspNetUsers_UserId");
-        });
-
-        modelBuilder.Entity<AspNetUserLogin>(entity =>
-        {
-            entity.HasKey(e => new { e.LoginProvider, e.ProviderKey, e.UserId }).HasName("PK_dbo.AspNetUserLogins");
-
-            entity.HasIndex(e => e.UserId, "IX_UserId");
-
-            entity.Property(e => e.LoginProvider).HasMaxLength(128);
-            entity.Property(e => e.ProviderKey).HasMaxLength(128);
-            entity.Property(e => e.UserId).HasMaxLength(128);
-
-            entity.HasOne(d => d.User).WithMany(p => p.AspNetUserLogins)
-                .HasForeignKey(d => d.UserId)
-                .HasConstraintName("FK_dbo.AspNetUserLogins_dbo.AspNetUsers_UserId");
-        });
-
-        modelBuilder.Entity<AspNetUserExtension>(entity =>
-        {
-            entity.ToTable(tb => tb.HasTrigger("AspNetUserExtensions_ClearExpired"));
-
-            entity.Property(e => e.Id).HasMaxLength(128);
-            entity.Property(e => e.PasswordResetTokenExpiration).HasColumnType("datetime");
-
-            entity.HasOne(d => d.IdNavigation).WithOne(p => p.AspNetUserExtension)
-                .HasForeignKey<AspNetUserExtension>(d => d.Id)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_AspNetUserExtensions_AspNetUsers");
-        });
-        */
 
         modelBuilder.Entity<Document>(entity =>
         {
